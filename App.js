e6d0614bcc65a51873c3f58e08770d43c2410aa4
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-10-15 22:48:00 +0200

    Add delete item functionality

--- App.js
+++ App.js
@@ -11,12 +11,18 @@ const App = () => {
     {id: 4, name: 'Juice'},
   ])
 
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id)
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List"/>
       <FlatList 
         data={items} 
-        renderItem={({item}) => <ListItem item={item} />}
+        renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem}/>}
       />
     </View>
   );
