b0a04cc3b46d48aef062fec46ecf7066f18e1183
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-10-15 19:47:49 +0200

    Display list of items

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = ({title}) => {
+
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkslateblue',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+  },
+})
+
+export default Header;
