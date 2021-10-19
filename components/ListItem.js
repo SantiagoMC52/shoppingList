4c838cffbfeb7f8423db7be3ea8eb1cf8cf4a004
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-10-19 14:22:31 +0200

    Remove import and refactor text style

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,4 +1,3 @@
-import { Linter } from 'eslint';
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
@@ -6,19 +5,17 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome';
 const ListItem = ({item, deleteItem, markAsDone}) => {
 
   const styleText = {
-    decoration: {
-      textDecorationLine: !item.done ? 'none' : 'line-through',
-    },
     listItemText: {
       fontSize: 18,
+      textDecorationLine: !item.done ? 'none' : 'line-through',
       fontWeight: 'bold',
-  },
+    },
   }
 
   return (
     <TouchableOpacity style={styles.listItem} onLongPress={() => markAsDone(item.id)}>
       <View style={styles.listItemView}> 
-        <Text style={{...styleText.listItemText, ...styleText.decoration}}>{item.text}</Text>
+        <Text style={styleText.listItemText}>{item.text}</Text>
         <View style={styles.listItemIcons}>
           <Icon 
               name='edit' 
