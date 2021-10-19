87538f65a7b10f4176bdcfe0144eff930b0ce940
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-10-19 16:18:03 +0200

    Remove unnecessary import

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,4 +1,3 @@
-import { placeholder } from '@babel/types';
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
